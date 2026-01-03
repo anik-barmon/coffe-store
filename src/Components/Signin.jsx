a5c0d284a38fcee75b0c5a28e2be14c0d5349062
sonjoybarman <sonjoybarmon19@gmail.com>
2026-01-03 22:02:56 +0600

    lastTime update complete

--- src/Components/Signin.jsx
+++ src/Components/Signin.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router";
+import { AuthContest } from "../contests/AuthContest";
 
 const Signin = () => {
+  const { signInUser } = useContext(AuthContest);
+
   const handleSignIn = (event) => {
     event.preventDefault();
     const form = event.target;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(email, password);
+    // console.log(email, password);
+    // Sign in user firebase
+    signInUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+        ///update last signin time
+        const signInInfo = {
+          email,
+          lastSignInTime: result.user?.metadata?.lastSignInTime,
+        };
+        /////////////////////
+        fetch("http://localhost:3000/users", {
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(signInInfo),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("after update patch", data);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
--- src/contests/AuthProvider.jsx
+++ src/contests/AuthProvider.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { AuthContest } from "./AuthContest";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../Firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
   const userInfo = {
     createUser,
+    signInUser,
   }; // Placeholder for user information and authentication logic
   return <AuthContest value={userInfo}>{children}</AuthContest>;
 };
